ccf57677c8909d8b6b6ecd92cea2ba4ddbdd1c58
nataliaftyv <nataliaftyv@users.noreply.github.com>
2021-04-29 10:20:00 +0200

    Create db and model

--- app.py
+++ app.py
@@ -1,4 +1,33 @@
-import peewee
+from peewee import *
+import datetime
 import csv
+
+db = SqliteDatabase('inventory.db')
+
+class Product(Model):
+    product_id = PrimaryKeyField()
+    product_name = CharField(max_length=255, unique=True)
+    product_quantity = IntegerField()
+    product_price = IntegerField()
+    date_updated = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+def initialize_db():
+    db.connect()
+    db.create_tables([Product], safe=True)
+
+
+def create_entry(productName, productQty, productPrice):
+    Product.create(product_name=productName, product_quantity=productQty, product_price=productPrice)
+
+initialize_db()
+
+
+
+
+
 
 
